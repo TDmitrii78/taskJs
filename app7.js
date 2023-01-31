4eaaa4c3ed677efc707eb690f05b36a1fa39ebbd
dmitrii <hdmail2022@gmail.com>
2023-01-31 23:18:49 +0400

    my task

--- app7.js
+++ app7.js
@@ -9,18 +9,18 @@
 //       return this.reduce((acc, item) => acc +item);
 //     }
 //   }
-  
+
 //   let arr = new sumArray(25, 7, 8, 11);
-  
+
 //   console.log(arr.sumArr()); 
-  
-  
+
+
 //   let  newArr = new sumArray();
-  
+
 //   newArr.push(1, 22, 5);
-  
+
 //   console.log(newArr.sumArr());
-  
+
 // ----------------------------------------------------
 
 // class MyTravels {
@@ -54,7 +54,7 @@
 
 //     }
 // }
- 
+
 // function dialogYesNo(question, yesNo) {
 //     for (;;) {
 //         let word = prompt(question, yesNo);
@@ -112,20 +112,20 @@
 
 //     alert(MyTrvls.personalData());
 //     flag = dialogYesNo(`${MyTrvls.location}. \nПереезжаем или останемся здесь? Да или Нет (отмена)`,"Да");
-   
+
 //     if (!flag) {
 //         let flag2 = dialogYesNo(`Показать список посещенных стран? Да или Нет (отмена)`,"Да");
 
 //         if (flag2) {
 //             listVisitCountry();
 //         }
-        
+
 //     } else {
 
 //         addCountry(); // Вводим название страны
 //         listVisitCountry();
 //     }
-    
+
 //     if (confirm("Завершить работу?")) {
 //         break;
 //     }    
@@ -138,14 +138,14 @@
 // let user = {
 //     name: "John",
 //     age: 30,
-  
+
 //     sayHi() {
-    
+
 //       alert(this.name);
 //     }
-  
+
 //   };
-  
+
 //   user.sayHi(); 
 
 
@@ -206,11 +206,11 @@
 //     courses: ['html', 'css', 'js'],
 //     wife: null
 //   };
-  
+
 //   let json = JSON.stringify(student);
-  
+
 //   alert(typeof json); 
-  
+
 //   alert(json);
 
 // ------------------------------------
@@ -248,7 +248,7 @@
 //     return function() {
 //         alert("hello");
 //     };
-    
+
 // }
 
 // let asd2 = asd();
@@ -263,9 +263,9 @@
 //     }
 //     return res;
 //   }
-     
+
 // console.log(getCount("aaase    fafvakvn"));
- 
+
 // ----------------------------------
 
 // function disemvowel(str) {
@@ -300,3 +300,167 @@
 
 
 // console.log(digitalRoot(456));
+
+// ----------------------------------------------
+
+// function arrayDiff(a, b) {
+//    return a.filter(el => !b.includes(el)); 
+// }
+
+// console.log(arrayDiff([1,2,2,2,3],[2]));
+
+// -----------------------------------------------
+
+
+// function likes(names) {
+//     let str = "likes this";
+//     let str2 = "like this";
+//     switch(names.length) {
+//         case 0: return "no one " + str;
+//         case 1: return `${names[0]} ${str}`;
+//         case 2: return `${names[0]} and ${names[1]} ${str2}`;
+//         case 3: return `${names[0]}, ${names[1]} and ${names[2]} ${str2}`;
+//         default: return `${names[0]}, ${names[1]} and ${names.length - 2} others ${str2}`;
+//     }
+// }
+
+// let names = [1, 2, 3, 4, 5];
+
+// console.log(likes(names));
+
+// --------------------------------------------------
+
+// function findOdd(A) {
+//     let item = new Set(A); 
+//     for (let key of item){
+//         if (A.filter(el => el === key).length % 2) {
+//             return key;
+//         };
+//     }
+// }
+
+
+// let A = [1,2,2,3,3,3,4,3,3,3,2,2,1];
+
+// console.log(findOdd(A));
+
+// ---------------------------------------------------
+
+// function squareDigits(num){
+//     num = num.toString();
+//     return +num.split("").map(el => el * el).join("");
+// }
+
+// console.log(squareDigits(44));
+
+// ---------------------------------------------------
+
+// function highAndLow(numbers) {
+//     numbers = numbers.split(" ").sort((a, b) => (a - b));
+//     return numbers = `${numbers[numbers.length - 1]} ${numbers[0]}`;
+// }
+
+// console.log(highAndLow("81 42 3 4 5")); 
+
+// ----------------------------------------------
+
+// function descendingOrder(n){
+//     return +n.toString().split("").sort((a, b) => b - a).join("");
+//   }
+//   console.log(descendingOrder(15780));
+
+// -------------------------------------------------
+
+// function createPhoneNumber(numbers) {
+//     return `(${numbers.slice(0, 3).join("")}) ${numbers.slice(3, 6).join("")}-${numbers.slice(6).join("")}`;
+// }
+
+// console.log(createPhoneNumber([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+// -------------------------------------------------
+
+// function getMiddle(s) {
+//     let x = s.length/2;
+
+//     if (s.length % 2) {
+//         return s.slice(Math.floor(x), x+1)
+//     } else {
+//         return s.slice(x-1, x+1);
+//     }
+// }
+
+// console.log(getMiddle("test"));
+// console.log(getMiddle("testing"));
+// console.log(getMiddle("middle"));
+// console.log(getMiddle("A"));
+
+// --------------------------------------------
+
+// function accum(s) {
+//     return s.split("").map((el, i) => { return el = el.toUpperCase() + el.repeat(i).toLowerCase() }).join("-");
+// }
+
+// console.log(accum("asdf"));
+// console.log(accum("WWWWWW"));
+// console.log(accum("asdf"));
+// console.log(accum("asdf"));
+
+// --------------------------------------------
+
+// var isSquare = function(n){
+//     let res = 0;
+//     let i = 0;
+//         while(res <= n) { 
+          
+//             res = i*i;
+//             if (res == n) {
+//                 return true;
+//             }
+//             i++;
+//         }
+//         return false;
+// }
+
+// console.log(isSquare(0));
+
+// ----------------------------------------------
+
+// function positiveSum(arr) {
+//     return arr.reduce((acc, el) => (el > 0) ? acc + el : acc + 0, 0);
+
+// }
+            
+
+// console.log(positiveSum([1,-4,7,12]));
+
+// ---------------------------------------------
+
+// function filter_list(l) {
+//     let set = new Set(l);
+//     let arr = [];
+//     for (let value of set) {
+//         if (typeof(value) === "number") {
+//             arr.push(value);
+//         }
+//     }
+//     return arr;
+// }
+
+// ---------------------------------------------------------
+
+// function filter_list(l) {
+//     return l.filter(function(v) {return typeof v == 'number'})
+//   }
+
+// console.log(filter_list([1,2,2,2,'a','b']));
+
+// ----------------------------------------------------------
+
+function isIsogram(str){
+  let set = new Set(str.toLowerCase().split(""));
+  return (set.size === str.length); 
+}
+
+
+
+console.log(isIsogram("asdfghA"));
