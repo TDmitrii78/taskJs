e15fca92cb34632194ea69af287b3dfd526cd5c2
dmitrii <hdmail2022@gmail.com>
2023-02-04 13:33:52 +0400

    my task

--- app8.js
+++ app8.js
@@ -205,18 +205,166 @@
 
 // -----------------------------------------------------
 
-function order(words){
-  return words.split(" ").sort((a, b) => {
-    a = a.split("").sort().join("");
+// function order(words){
+//   return words.split(" ").sort((a, b) => {
+//     a = a.split("").sort().join("");
  
-    b = b.split("").sort().join("");
+//     b = b.split("").sort().join("");
   
-    if (a > b) return +1;
-    if (a === b) return 0;
-    if (a < b) return -1;
-  })
-  .join(" ");
+//     if (a > b) return +1;
+//     if (a === b) return 0;
+//     if (a < b) return -1;
+//   })
+//   .join(" ");
+// }
+
+// console.log(order("is2 Thi1s T4est 3a"));  //-->  "Thi1s is2 3a T4est"
+// order("4of Fo1r pe6ople g3ood th5e the2");  //-->  "Fo1r the2 g3ood 4of th5e pe6ople"
+
+// ------------------------------------------------------------------------------
+
+// function tribonacci(signature, n){
+//   let num;
+//   if (n === 0) {return []}
+//   if (n === 1) {return [1]}
+//   for (let i = 4; i <= n; i++) {
+//     num = (signature.slice(-3).reduce((acc, el) => acc + el));
+//     signature.push(num);
+//   }
+//   return signature;
+// }
+
+  
+// console.log(tribonacci( [300,200,100], 0)); //[0,0,1,1,2,4,7,13,24,44]   
+
+// ------------------------------------------------------------------------------
+
+// var uniqueInOrder=function(iterable){
+//   if (!Array.isArray(iterable)) {
+//     iterable = iterable.split("");
+//   }
+//   return iterable.filter((el, index, arr) => {
+//     if (el != arr[index+1]) {
+//       return el;
+//     }
+//   })
+// }
+// console.log(uniqueInOrder('AAAABBBCCDAABBB') );
+// console.log(uniqueInOrder('ABBCcAD')); 
+// console.log(uniqueInOrder([1,2,2,3,3])  );  
+
+// -------------------------------------------
+
+// function narcissistic(value) {
+//   return !(value - value.toString().split("").map((el, index, array) => {
+//     let res = el;
+//     for (let i = 1; i < array.length; i++) {
+//     res = res*el;
+//     }
+//     return res;
+//   }).reduce((acc, el) => acc + el));
+// }
+
+// console.log(narcissistic( 153 ));
+
+// ---------------------------------------------
+
+// function toCamelCase(str) {
+//   if (str === "") {
+//     return "";
+//   }
+//   return str.replaceAll("_", "-").split("-").map((el, index, arr) => {
+//     if ((arr.length === 3) && (arr[0].length === 1) && (arr[1].length === 1) && (arr[2].length === 1)) {
+//       return el.toUpperCase();
+//     } else {
+//       if (index === 0) {
+//         return el;
+//       } else {
+//         return el[0].toUpperCase() + el.slice(1);
+//       }
+//     }
+//   }).join("");
+// }
+
+
+// console.log(toCamelCase("The_stealth-warrior"));
+// console.log(toCamelCase("the_stealth_warrior"));
+// console.log(toCamelCase("A_b_v"));
+// console.log(toCamelCase(""));
+
+
+// -----------------------------------------------------------
+
+// function moveZeros(arr) {
+//   let len = arr.length;
+//   arr = arr.filter((el) => el !== 0); 
+//   for (let i = arr.length; i < len; i++) {
+//     arr.push(0);
+//   }
+//   return arr;
+// }
+
+
+// console.log(moveZeros([false,1,0,1,2,0,1,3,"a"]));
+
+// ----------------------------------------
+
+// function pigIt(str){
+//   return str.split(" ").map((el, index, arr) => {
+//     if ((el === "!") || (el === ".") || (el === ",") || (el === "?")) {
+//       return el;
+//     }
+//     return el.slice(1) + el[0] + "ay"
+//   }).join(" ");
+// }
+
+// console.log(pigIt('Pig latin is cool'));
+// console.log(pigIt('Hello world !'));
+
+// ------------------------------------------
+
+// function rgb(r, g, b){
+//   return [r, g ,b].map((el) => {
+//     if (el >= 255) {
+//       return "FF";
+//     }
+//     if (el <= 0) {
+//       return "00"
+//     }
+//     el = el.toString(16).toUpperCase();
+//     return (el.length < 2) ? "0" + el : el;
+//   }).join("");
+// }
+
+// console.log(rgb(173, 12, 25));
+
+// ----------------------------------------
+
+function rot13(message){
+  return message.split(" ").map((el) => {
+    let str = "";
+    for (let i = 0; i < el.length; i++) {
+    let charCod ="";
+      if ((el.codePointAt(i) >= 65) && (el.codePointAt(i) <= 90)) {
+        charCod = +el.codePointAt(i) - 64 + 13;
+        if (charCod > 26) {
+          charCod = charCod - 26;
+        } 
+        charCod += 64;  
+      } else if ((el.codePointAt(i) >= 97) && (el.codePointAt(i) <= 122)) {
+        charCod = +el.codePointAt(i) - 96 + 13;
+        if (charCod > 26) {
+          charCod = charCod - 26;
+        }
+        charCod += 96; 
+      } else {
+      charCod = +el.codePointAt(i);
+      }
+      str = str + String.fromCodePoint(charCod);
+    }
+    return str;
+  }).join(" ");
 }
 
-console.log(order("is2 Thi1s T4est 3a"));  //-->  "Thi1s is2 3a T4est"
-// order("4of Fo1r pe6ople g3ood th5e the2");  //-->  "Fo1r the2 g3ood 4of th5e pe6ople"
+console.log(rot13("Test failed with messsage."));
+console.log(rot13("0+2 is twelve."));
