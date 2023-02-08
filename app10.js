9e6c52485c1141fb05e1045eecd37a483a6282b1
dmitrii <hdmail2022@gmail.com>
2023-02-09 01:59:37 +0400

    my task

--- app10.js
+++ app10.js
@@ -1,18 +1,65 @@
-function loadScript(src, callback) {
-    let script = document.createElement('script');
-    script.src = src;
-    script.onload = () => callback(script);
-    document.head.append(script);
-  }
+// function loadScript(src, callback) {
+//     let script = document.createElement('script');
+//     script.src = src;
+//     script.onload = () => callback(script);
+//     document.head.append(script);
+//   }
   
-  loadScript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js', script => {
-    alert(`Здорово, скрипт ${script.src} загрузился`);
-    alert( _ ); // функция, объявленная в загруженном скрипте
-  });
-
+//   loadScript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js', script => {
+//     alert(`Здорово, скрипт ${script.src} загрузился`);
+//     alert( _ ); 
+//   }
 //   -----------------------------------------------
 
-let promise = new Promise(function(resolve, reject) {
-    // функция-исполнитель (executor)
-    // "певец"
-  });
+// let promise = new Promise(function(resolve, reject) {
+//   alert("123");
+//   setTimeout(() => resolve("done"), 1000);
+// });
+
+// let myPromise = new Promise((resolve, reject) => {
+//     //resolve("Done");
+//     reject("Error");
+// })
+
+// myPromise
+//     .then(value => alert("done"))
+//     .catch(error => alert("Error"))
+
+// console.log(myPromise);
+
+// fetch('https://jsonplaceholder.typicode.com/posts/1')
+//   .then((response) => {
+//     console.log(response);
+//     return response.json()})
+//   .then((json) => console.log(json));
+
+
+// ------------------------------------
+
+function findEvenIndex(arr) {
+  if (arr.length < 3) {
+    return -1;
+  }
+  for (let i = 1; i < arr.length - 2; i++) {
+    let arrLeft = [];
+    let arrRight = [];
+    arr.forEach((element, index) => {
+      if (index < i) {
+        arrLeft.push(arr[index]);
+      }
+      if (index > i) {
+        arrRight.push(arr[index]);
+      }
+    });
+    if (arrLeft.reduce((acc, el) => acc + el, 0) === arrRight.reduce((acc, el) => acc + el, 0)) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+//console.log(findEvenIndex([20,10,-80,10,10,15,35]));
+console.log(findEvenIndex([1,2,3,4,3,2,1]));
+console.log(findEvenIndex([1,100,50,-51,1,1]));
+console.log(findEvenIndex([1,2,3,4,5,6]));
+console.log(findEvenIndex([20,10,30,10,10,15,35]));
--- app9.js
+++ app9.js
@@ -46,3 +46,132 @@
 
 // --------------------------------------------------------
 
+// function solution(number){
+//     if (number <= 0) {
+//         return 0;
+//     }
+//     let arr = [];
+//     let five = Math.floor(number / 5); 
+//     if (!(number % 5)) {
+//         five = five - 1;
+//     } 
+//     let three = Math.floor(number / 3); 
+//     if (!(number % 3)) {
+//         three = three - 1;
+//     } 
+//     for (let i = 1; i <= five; i++) {
+//         arr.push(5 * i);
+//     }  
+//     for (let i = 1; i <= three; i++) {
+//         arr.push(3 * i);
+//     }
+//     let numSet = new Set(arr);
+//     arr=[];
+//     numSet.forEach(el => arr.push(el));
+//     return arr.reduce((acc, el) => acc + el, 0);
+// }
+
+// function solution(number){
+//     var sum = 0;
+//     for (var i = 3; i < number; i++) {
+//       if (i % 3 == 0 || i % 5 == 0) {
+
+//         sum += i;
+//         console.log(sum);
+//       }
+//     }
+//     return sum;
+//   }
+
+// console.log(solution(10));
+
+// ------------------------------------------------------------
+
+// fetch(`https://api.github.com/users/TDmitrii78`)
+// .then(response => response.json())
+// .then(githubUser => {
+//   let img = document.createElement('img');
+//   img.src = githubUser.avatar_url;
+//   img.className = "promise-avatar-example";
+//   document.body.append(img);
+// })
+// .catch(error => console.log("error"));
+
+// -------------------------------------------------------------
+
+// function digPow(n, p){
+//   let arr =[];
+//   if (n < 9) {
+//     arr.push(n.toString());
+//   } else {
+//     arr = n.toString().split("");
+//   }
+//   let res = arr.reduce((acc, el, index, arr) => acc + el ** (p + index), 0);
+//   if ((res < 1) || (res % n)) {
+//     return -1;
+//   }
+//   res = res/n;
+//   return res;
+// }
+
+// console.log(digPow(-4, 3));
+
+// --------------------------------------------------------------
+
+// function isPangram(string) {
+
+//   let stringSet = new Set(string.toLowerCase().split("").filter(el => {
+//     if ((el.codePointAt(0) >= 97) && (el.codePointAt(0) <= 122)) {
+//       return true;
+//     }
+//     return false;
+//   }));
+//   if (stringSet.size === 26) {
+//     return true;
+//   }
+//   return false;
+// }
+// console.log(isPangram("The quick brown fox jumps over the lazy dog."));
+
+// -------------------------------------------------------------------
+
+// document.body.style.background = 'red'; // сделать фон красным
+
+// setTimeout(() => document.body.style.background = '', 3000);
+// 
+// ------------------------------------------------------------
+
+// let promise = new Promise(function(resolve, reject) {
+ 
+//     setTimeout(() => resolve("Its OK"), 1000);
+// })
+// .then(result2 => console.log(result2));
+
+// console.log(promise);
+
+
+// fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits')
+//   .then(response => response.json())
+//   .then(commits => console.log(commits));
+
+
+// fetch('https://jsonplaceholder.typicode.com/posts/1')
+
+// .then(response => console.log(response.blob()));
+
+
+fetch('https://avatars.githubusercontent.com/u/118843499?v=4')
+.then(response => response.blob)
+.then(pict => {
+let img = document.createElement('img')
+img.style = 'position:fixed;top:10px;left:10px;width:100px';
+document.body.append(img);
+
+img.src = pict;
+  setTimeout(() => { 
+  img.remove();
+  URL
+ }, 3000
+)
+})
+.catch(error => console.log("error"));
