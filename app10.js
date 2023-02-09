442f683dabd5433f90b8f1eba4b6b5750947f556
dmitrii <hdmail2022@gmail.com>
2023-02-09 23:02:46 +0400

    my task

--- app10.js
+++ app10.js
@@ -36,30 +36,74 @@
 
 // ------------------------------------
 
-function findEvenIndex(arr) {
-  if (arr.length < 3) {
-    return -1;
-  }
-  for (let i = 1; i < arr.length - 2; i++) {
-    let arrLeft = [];
-    let arrRight = [];
-    arr.forEach((element, index) => {
-      if (index < i) {
-        arrLeft.push(arr[index]);
-      }
-      if (index > i) {
-        arrRight.push(arr[index]);
-      }
-    });
-    if (arrLeft.reduce((acc, el) => acc + el, 0) === arrRight.reduce((acc, el) => acc + el, 0)) {
-      return i;
-    }
-  }
-  return -1;
-}
+// function findEvenIndex(arr) {
+//   if (arr.length < 3) {
+//     return -1;
+//   }
+//   for (let i = 1; i < arr.length - 2; i++) {
+//     let arrLeft = [];
+//     let arrRight = [];
+//     arr.forEach((element, index) => {
+//       if (index < i) {
+//         arrLeft.push(arr[index]);
+//       }
+//       if (index > i) {
+//         arrRight.push(arr[index]);
+//       }
+//     });
+//     if (arrLeft.reduce((acc, el) => acc + el, 0) === arrRight.reduce((acc, el) => acc + el, 0)) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
 
-//console.log(findEvenIndex([20,10,-80,10,10,15,35]));
-console.log(findEvenIndex([1,2,3,4,3,2,1]));
-console.log(findEvenIndex([1,100,50,-51,1,1]));
-console.log(findEvenIndex([1,2,3,4,5,6]));
-console.log(findEvenIndex([20,10,30,10,10,15,35]));
+// //console.log(findEvenIndex([20,10,-80,10,10,15,35]));
+// console.log(findEvenIndex([1,2,3,4,3,2,1]));
+// console.log(findEvenIndex([1,100,50,-51,1,1]));
+// console.log(findEvenIndex([1,2,3,4,5,6]));
+// console.log(findEvenIndex([20,10,30,10,10,15,35]));
+
+// --------------------------------------------------
+
+// fetch('https://jsonplaceholder.typicode.com/posts/1')
+// .then(response => response.json())
+// .then(json => console.log(json.body))
+
+// ---------------------------------------------------
+
+// function XO(str) {
+//     let resX = 0;
+//     let resO = 0;
+//     str = str
+//     .toLowerCase()
+//     .split("")
+//     .forEach(el => {
+//         if (el === "x") {
+//             ++resX;
+//         }
+//         if (el === "o") {
+//             ++resO;
+//         }
+//     })
+//     if ((resX === resO)  || ((!resX) && (!resO))){
+//         return true;
+//     }
+//     return false;
+// }
+
+// console.log(XO('xo'));    //,true);
+// console.log(XO("xxOo"));  //,true);
+// console.log(XO("xxxm"));  //,false);
+// console.log(XO("Oo"));    //,false);
+// console.log(XO("ooom"));  //,false);
+
+// ---------------------------------------------------
+
+String.prototype.toJadenCase = function (str) {
+    return str.split(" ").map(el => el[0].toUpperCase() + el.slice(1)).join(" ");
+  };
+
+  var str = "How can mirrors be real if our eyes aren't real";
+
+  console.log(String.prototype.toJadenCase(str));
